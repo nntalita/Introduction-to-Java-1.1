fb5e505e6b719835e98ae5ff3faec8b301274a06
Natalya Pletneva <nntalita@gmail.com>
2021-09-11 15:44:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int currentBalance = 2_000_000_000;
+        int transfer = 500_000_000;
+        int finalBalance = currentBalance + transfer;
+        System.out.println(finalBalance);
+    }
+
+}
+
